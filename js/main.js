4e058db0699cbccab6bb2d594543c3dbc77d4f44
Nastya <asekerich99@gmail.com>
2021-04-20 00:54:43 +0300

    fix: styles and slider 1000px

--- js/main.js
+++ js/main.js
@@ -1,24 +1,30 @@
 const secondarySliderOptions = {
-  rewind: false,
+  rewind: true,
   fixedWidth: 90,
   fixedHeight: 80,
   gap: 12,
   focus: 'left',
   arrows: false,
   easing: 'linear',
-  autoplay: true,
   pagination: false,
   pauseOnHover: true,
   drag: true,
   isNavigation: true,
   cover: true,
   margin: {
-    top : '10px',
+    top: '10px',
   },
   direction: 'ltr',
+  breakpoints: {
+    '1024': {
+      fixedWidth: 60,
+      fixedHeight: 50,
+    }
+  },
 };
 
 const primarySliderOptions = {
+  rewind: true,
   type: 'fade',
   fixedWidth: 570,
   fixedHeight: 336,
@@ -26,21 +32,27 @@ const primarySliderOptions = {
   arrows: true,
   cover: true,
   isNavigation: true,
+  breakpoints: {
+    '1024': {
+      fixedWidth: 450,
+      fixedHeight: 288,
+    }
+  },
 };
 
-document.addEventListener( 'DOMContentLoaded', function () {
-	var secondarySlider = new Splide( '#secondary-slider', secondarySliderOptions ).mount();
-	var primarySlider = new Splide( '#primary-slider', primarySliderOptions );
-	
-	primarySlider.sync( secondarySlider ).mount();
+document.addEventListener('DOMContentLoaded', function () {
+  var secondarySlider = new Splide('#secondary-slider', secondarySliderOptions).mount();
+  var primarySlider = new Splide('#primary-slider', primarySliderOptions);
 
-  var secondarySlider2 = new Splide( '#secondary-slider-2', secondarySliderOptions ).mount();
-	var primarySlider2 = new Splide( '#primary-slider-2', primarySliderOptions );
-	
-	primarySlider2.sync( secondarySlider2 ).mount();
+  primarySlider.sync(secondarySlider).mount();
 
-  var secondarySlider3 = new Splide( '#secondary-slider-3', secondarySliderOptions ).mount();
-	var primarySlider3 = new Splide( '#primary-slider-3', primarySliderOptions );
-	
-	primarySlider3.sync( secondarySlider3 ).mount();
-} );
+  var secondarySlider2 = new Splide('#secondary-slider-2', secondarySliderOptions).mount();
+  var primarySlider2 = new Splide('#primary-slider-2', primarySliderOptions);
+
+  primarySlider2.sync(secondarySlider2).mount();
+
+  var secondarySlider3 = new Splide('#secondary-slider-3', secondarySliderOptions).mount();
+  var primarySlider3 = new Splide('#primary-slider-3', primarySliderOptions);
+
+  primarySlider3.sync(secondarySlider3).mount();
+});
