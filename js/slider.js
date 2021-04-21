b3dfcbd7097fb0213aae48e8542ee3f0c28e7c8f
Nastya <asekerich99@gmail.com>
2021-04-21 05:57:23 +0300

    add: slider in example section

---
+++ js/slider.js
@@ -0,0 +1,31 @@
+const btnNext = document.getElementById('next');
+const btnPrev = document.getElementById('prev');
+const slider = document.querySelector('div.slider');
+const sliderCount = document.querySelector('.slider__count');
+let viewSlide = 0;
+let viewport;
+
+window.addEventListener('resize', () => {
+  viewport = document.querySelector('.slider__mobile').offsetWidth;
+  slider.style.left = -viewSlide * viewport + 'px';
+});
+
+btnNext.addEventListener('click', function () {
+  if (viewSlide < 2) { 
+      viewSlide++;
+  } else {
+      viewSlide = 0;
+  }
+  slider.style.left = -viewSlide * viewport + 'px';
+  sliderCount.innerText = `0${viewSlide +1 }/03`;
+});
+
+btnPrev.addEventListener('click', function () {
+  if (viewSlide > 0) { 
+      viewSlide--; 
+  } else {
+      viewSlide = 2; 
+  }
+  slider.style.left = -viewSlide * viewport + 'px';
+  sliderCount.innerText = `0${viewSlide +1 }/03`;
+});
