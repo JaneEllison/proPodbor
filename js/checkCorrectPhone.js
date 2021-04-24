94c39a3598ed132a5bfd73a208eb4296033188d2
Nastya <asekerich99@gmail.com>
2021-04-24 23:09:07 +0300

    fix:checkCorrectForm

--- js/checkCorrectPhone.js
+++ js/checkCorrectPhone.js
@@ -4,25 +4,26 @@ const questionFormError = document.querySelectorAll('.form-error');
 
 const pattern = new RegExp('^[+]375 [0-9]{2} *[0-9]{3} *[0-9]{2} *[0-9]{2}$');
 
-const setDefaultValue = () => {
-  input.addEventListener('focus', () => {
-    input.value = '+375 '
-  });
-};
-
 const addErrorStyles = () => {
   questionFormError.forEach((error) => error.innerText = 'Номер телефона введен некорректно');
-  input.style.borderBottomColor = '#F44D34';
 };
 
 const checkCorrectPhone = () => {
   phone.forEach((input) => {
-    setDefaultValue();
+    input.addEventListener('focus', () => {
+      input.value = '+375 '
+    });
+
     input.addEventListener('change', () => {
       const check = pattern.test(input.value);
-      if (!check) addErrorStyles
+      if (!check) {
+        input.style.borderBottomColor = '#F44D34'
+        addErrorStyles();
+      } else {
+        ''
+      }
     });
   });
 };
 
-
+checkCorrectPhone();
