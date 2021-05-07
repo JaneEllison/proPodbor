b969c0f71ddf5952468ebadd42054e81b14bb3da
Nastya <asekerich99@gmail.com>
2021-05-07 17:28:24 +0300

    add: scroll for anchors, fix description items block

--- js/changeDescriptionItems.js
+++ js/changeDescriptionItems.js
@@ -1,25 +1,46 @@
 const descriptionAddItemsButton = document.querySelector('.description__add_items-block');
-const descriptionInfoItems = document.querySelectorAll('.hidden-item');
+const descriptionRemoveItemsButton = document.querySelector('.description__hide_items-block');
+const hiddenBlock = document.querySelector('.hidden__block');
 const descriptionDownArrow = document.querySelector('.description_down-arrow');
 
 const changeDescriptionAddItems = () => {
   if (window.matchMedia('(max-width: 980px)').matches) {
-    descriptionInfoItems.forEach((item) => {
-      item.classList.add('description__info_item-hide');
-    });
-    descriptionDownArrow.classList.add('description_down-arrow-hide')
+    hiddenBlock.classList.remove('show');
+    descriptionAddItemsButton.classList.remove('hide');
+    descriptionRemoveItemsButton.classList.add('hide');
   };
 };
 
-changeDescriptionAddItems();
-
 descriptionAddItemsButton.addEventListener('click', () => {
-  descriptionInfoItems.forEach((item) => {
-    item.classList.toggle('description__info_item-hide');
-  });
-  descriptionDownArrow.classList.toggle('description_down-arrow-hide')
+  hiddenBlock.classList.toggle('show');
+  descriptionAddItemsButton.classList.toggle('hide');
+  descriptionRemoveItemsButton.classList.toggle('hide');
 });
+
+descriptionRemoveItemsButton.addEventListener('click', () => {
+  hiddenBlock.classList.toggle('show');
+  descriptionAddItemsButton.classList.toggle('hide');
+  descriptionRemoveItemsButton.classList.toggle('hide');
+});
+
 
 window.addEventListener('resize', () => {
   changeDescriptionAddItems();
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  changeDescriptionAddItems();
+})
+
+
+
+
+
+
+
+
+
+
+
+// 2. Добавить анимацию при скролле на якорях
+// 3. Посмотреть еще раз в слайдере интеграцию с свайпом пальцем
